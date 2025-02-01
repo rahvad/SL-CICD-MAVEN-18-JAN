f49dbfd3f60b9c1f519f116381b1c459726328d3
Hitesh Kumar Sharma <hkshitesh@gmail.com>
2025-02-01 23:18:30 +0530

    even() fun added

--- src/main/java/mypkg/MyCalc.java
+++ src/main/java/mypkg/MyCalc.java
@@ -17,6 +17,14 @@ public class MyCalc {
 	{
 		return a/b;
 	}
+	public boolean evenNum(int a){
+		if(a%2 == 0){
+		   return true;
+		}
+		else {
+		   return false;
+		}
+	}
 
 	public static void main(String[] args) {
 		MyCalc calc = new MyCalc();
@@ -24,6 +32,7 @@ public class MyCalc {
 		System.out.println("Diff is: "+calc.diff(20, 10));
 		System.out.println("Mul is: "+calc.mul(20, 10));
 		System.out.println("Div is: "+calc.div(20, 10));
+		System.out.println("Even: "+calc.evenNum(10));
 	}
 
 }
